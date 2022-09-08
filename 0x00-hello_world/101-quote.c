c014245b3b124096531c716905bacf22358d91e2
Florence93 <idowuflorence93@gmail.com>
2022-09-08 07:21:51 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,18 +5,17 @@
 #include <unistd.h>
 
 /**
- *
  *  * main - Prints out the last part of a quote in the standard error.
+ * Description: using the main function "and that piece of art
+ * Return: 1 if success.
  *
- *   * Return: 1 if success.
- *
- *    */
+ */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
