d666aebb84a58f993b3f8898db13011d7bc88fee
Florence93 <idowuflorence93@gmail.com>
2022-09-09 05:52:48 -0700

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print letters without e and q
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z'; lett++)
+	{
+		if (lett != 'e' && lett != 'q')
+	{
+		putchar (lett)
+	}
+
+	}
+
+	putchar ('\n')
+}
