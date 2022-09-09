9558fc047ea0b9372b0d9c92da1fbb8e556fa896
Florence93 <idowuflorence93@gmail.com>
2022-09-09 06:18:04 -0700

    numbers to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from base 10 starting from 0
+ *
+ * Description: print numbers from base 10
+ * starting from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
