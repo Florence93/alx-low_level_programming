21fb1e2ad350f9c438224477ea448d2d5a389fe7
Florence93 <idowuflorence93@gmail.com>
2022-09-09 05:00:25 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - write program that prnts alphabet letters
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
