88477c57fbc2f5babe2a00fc7cc58b8c6e551f26
Florence93 <idowuflorence93@gmail.com>
2022-09-09 08:54:55 -0700

    change

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - print out the code for theis task
+ *
+ * Description: print out the code for this task
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int m_d;
+	int l_d;
+
+	while (c <= 999)
+	{
+
+		f_d = (c / 100 + '0');
+		m_d = (c / 10 % 10 + '0');
+		l_d = (c % 10 + '0');
+
+
+		if ((f_d < m_d) && (m_d < l_d))
+		{
+			putchar(f_d);
+			putchar(m_d);
+			putchar(l_d);
+
+			if (c != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+		c++;
+
+}
+	putchar('\n');
+	return (0);
+}
