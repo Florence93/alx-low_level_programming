0b4637fe81cda3ab2df77df0841c9c7a1c3fab83
Florence93 <idowuflorence93@gmail.com>
2022-09-09 04:28:13 -0700

    betty style change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,13 +6,10 @@
 
 
 
-/***
+/**
  * main - print if the number is postive, zero, or negative
  *
- *
- *
- * Description: using the main function
- *
+ * Description: using the main function,
  * this program prints "Programming is positive, zero, or negative
  *
  * Return: 0
