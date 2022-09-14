d0937c1ce943e5203b7a24de83875800f94e7f1b
Florence93 <idowuflorence93@gmail.com>
2022-09-14 04:38:24 -0700

    change

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,21 +2,36 @@
 
 /**
 * main - causes an infinite loop
+* while- decribe conditional statement
 * Return: 0
 */
 
 int main(void)
+
 {
+
 	int i;
 
+
+
 	printf("Infinite loop incoming :(\n");
+
+
+
 	i = 0;
-/*
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+
+	/**
+	* while (i < 10)
+	* {
+	*putchar(i);
+	* }
+	*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
+
+
 	return (0);
+
 }
+
