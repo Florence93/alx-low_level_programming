83b320803be77f19ce47397918940ee0a3638715
Florence93 <idowuflorence93@gmail.com>
2022-10-03 00:59:50 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,28 +7,43 @@
  * Return: Pointer to new string
  **/
 
-ar *argstostr(int ac, char **av)
+char *argstostr(int ac, char **av)
 {
-	char *str;
-	int arg, byte, index, size = ac;
+	char *aout;
+	int c, i, j, ia;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (arg = 0; arg < ac; arg++)
+	if (ac == 0)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
-			size++;
-	}
-	str = malloc(sizeof(char) * size + 1);
-	if (str == NULL)
 		return (NULL);
-	index = 0;
-	for (arg = 0; arg < ac; arg++)
-	{
-		for (byte = 0; av[arg][byte]; byte++)
-			str[index++] = av[arg][byte];
-		str[index++] = '\n';
 	}
-	str[size] = '\0';
-	return (str);
+	for (c = i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
+	aout = malloc((c + 1) * sizeof(char));
+
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
+	return (aout);
 }
