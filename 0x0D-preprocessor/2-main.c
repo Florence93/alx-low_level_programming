8d20b39895fe6895665428b954848aed68ec436d
Florence93 <idowuflorence93@gmail.com>
2022-10-10 00:48:44 -0700

    change

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the codes for the Holberton School students
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+printf("%s\n", __FILE__);
+
+return (0);
+
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FILE_ABS
+
+#define FILE_ABS
+
+
+
+#define ABS(X) ((X) < 0 ? -1 * (X) : (X))
+
+
+
+#endif
